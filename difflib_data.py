0d3b6f5182fd37e3815086ce79b3abf3e7f1d60c
HCH1 <theone_730801@hotmail.com>
2016-10-26 19:23:29 +0800

    Add files via upload

---
+++ difflib_data.py
@@ -0,0 +1,7 @@
+file = open('/Users/HCHung/Documents/2016demo/demo1013SOPcleanDMforDiff/o1', "r")    
+text1 = file.read()
+text1_lines = text1.splitlines()
+
+file2 = open('/Users/HCHung/Documents/2016demo/demo1013SOPcleanDMforDiff/o2', "r")    
+text2 = file2.read()
+text2_lines = text2.splitlines()
