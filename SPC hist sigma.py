e0fa4c0e31c89ed69dd9e43a028d33541c0373bd
HCH1 <theone_730801@hotmail.com>
2016-10-26 20:59:20 +0800

    Update SPC hist sigma.py

--- SPC hist sigma.py
+++ SPC hist sigma.py
@@ -26,3 +26,20 @@ ax.set_xticks(bins)
 fig.savefig("/tmp/out.png")
 
 plt.show()
+## http://matplotlib.org/users/pyplot_tutorial.html
+import numpy as np
+import matplotlib.pyplot as plt
+
+mu, sigma = 100, 15
+x = mu + sigma * np.random.randn(10000)
+
+# the histogram of the data
+n, bins, patches = plt.hist(x, 50, normed=1, facecolor='g', alpha=0.75)
+
+plt.xlabel('Smarts')
+plt.ylabel('Probability')
+plt.title('Histogram of IQ')
+plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
+plt.axis([40, 160, 0, 0.03])
+plt.grid(True)
+plt.show()
