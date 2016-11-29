06dd1689c16473e8212cbbf75883320b43dce9de
HCH1 <theone_730801@hotmail.com>
2016-11-29 14:19:27 +0800

    Add files via upload

---
+++ demo1129MCplotv2.R
@@ -0,0 +1,12 @@
+mydata = read.csv("1129demov2.csv")
+myans1 <- summary(mydata)
+write.table(x = myans1, file = "o1129v1.csv", sep = ",", col.names = NA, qmethod = "double")
+# library("stats")
+md_col1_sd <- sd(mydata$AMinTopChord)
+md_col1_p3sigma <- mean(mydata$AMinTopChord) + 3*md_col1_sd
+md_col1_m3sigma <- mean(mydata$AMinTopChord) - 3*md_col1_sd
+# library("ggplot2")
+d <- density(mydata$AMinTopChord) # returns the density data
+hist(mydata$AMinTopChord, prob=TRUE)
+lines(d) # use lines for the second one
+# plot(d) # plots the results
