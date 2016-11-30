18c290abb7f3ac4715d4e6152cd19dd3e9fdce1b
HCH1 <theone_730801@hotmail.com>
2016-11-30 11:15:23 +0800

    Update demo1129MCfuncv3.R

--- demo1129MCfuncv3.R
+++ demo1129MCfuncv3.R
@@ -2,6 +2,7 @@ mydata = read.csv("1129demov2.csv")
 ##
 MySummary <- function(n)
 {
+  len <- length(n);
   max <- max(n);
   med <- median(n);
   avg <- mean(n);
@@ -9,10 +10,10 @@ MySummary <- function(n)
   stdv <- sd(n);
   p3sigma <- mean(n) + 3*sd(n);
   m3sigma <- mean(n) - 3*sd(n);
-  mysum_ans <- rbind(max, med, avg, min, stdv, p3sigma, m3sigma);
+  mysum_ans <- rbind(len, max, med, avg, min, stdv, p3sigma, m3sigma);
   write.table(x = mysum_ans, file = "o1129v2.csv", sep = ",", col.names = NA, qmethod = "double");
   den <- density(n);
-  hist(n, prob=TRUE);
+  hist(n);
   # lines(den)
   return(mysum_ans)
 }
