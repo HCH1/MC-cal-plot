178048986a79f9663110c7c81dc62bee49cb1f18
HCH1 <theone_730801@hotmail.com>
2016-12-08 21:39:26 +0800

    Update demo1208TT_tou_porh.R

--- demo1208TT_tou_porh.R
+++ demo1208TT_tou_porh.R
@@ -1,12 +1,49 @@
+# install.packages("reshape2")
+# install.packages("compare")
+# library(reshape2)
+# library(compare)
+## https://www.r-bloggers.com/reshape-and-aggregate-data-with-the-r-package-reshape2/
+## http://seananderson.ca/2013/10/19/reshape.html
+mx_dtt = read.csv("28LPSe_Rev1.0_1.0_Truth_Tables_DTTn1v3.csv", header = TRUE)
+# unpivot
+mx_dtt2 <- melt(mx_dtt, id = c("Device", "SPICE"), na.rm = TRUE)
+write.csv(x = mx_dtt2, file = "o1208_mx_dtt2.csv")
+## http://stackoverflow.com/questions/13967063/remove-duplicate-rows-in-r
+## http://stackoverflow.com/questions/8661514/concatenating-n-columns-of-text-in-r
+## de-couple
+mx_dtt2_coldev2spi <- unique( paste(mx_dtt2$Device, mx_dtt2$SPICE, sep = " | ") ) 
+write.csv(x = mx_dtt2_coldev2spi, file = "o1208_mx_dtt2_coldev2spi.csv")
+
+mx_dtt2_coldev <- unique(mx_dtt2$Device)
+write.csv(x = mx_dtt2_coldev, file = "o1208_mx_dtt2_coldev.csv")
+
+mx_dtt2_colspi <- unique(mx_dtt2$SPICE)
+write.csv(x = mx_dtt2_colspi, file = "o1208_mx_dtt2_colspi.csv")
+
+mx_dtt2_colvar <- unique(mx_dtt2$variable)
+write.csv(x = mx_dtt2_colvar, file = "o1208_mx_dtt2_colvar.csv")
+# sort
+# mx_dtt2 <- mx_dtt2[order(mx_dtt2$Device),]
+# pivot back
+mx_dtt3 <- dcast(mx_dtt2, Device + SPICE ~ variable)
+write.csv(x = mx_dtt3, file = "o1208_mx_dtt3.csv")
+## target -> find devices
+mx_dtt2_fil1 <- mx_dtt2[mx_dtt2$variable == "CELLSNR",]
+mx_dtt2_fil1 <- mx_dtt2_fil1[mx_dtt2_fil1$value != "0",]
+write.csv(x = mx_dtt2_fil1, file = "o1208_mx_dtt2_fil1.csv")
+mx_dtt2_fil2 <- unique(mx_dtt2_fil1$Device)
+write.csv(x = mx_dtt2_fil2, file = "o1208_mx_dtt2_fil2.csv")
 ##
-mx_dtt = read.csv("28SLP_Rev1.1_4.0_Truth Table.csv")
-mx_dtt_unpiv = unpivot(mx_dtt) # table will be "device layer value"
-##
-DemoTT_tou_proh <- function(lay1, val1, lay2, val2)
-{
-mx_dtt_unpiv[$Layer == lay1, $Value != val1]
+mx_dtt2_fil_oppo <- mx_dtt2[]
 
-mx_dtt_unpiv2 <- cbind(mx_dtt_unpiv, if(mx_dtt_unpiv[,2]==lay1 and ))
-
-
+for(i in 1:length(mx_dtt2_fil2)) {
+  mx_dtt2_fil_oppo <- mx_dtt2_fil_oppo[mx_dtt2_fil_oppo$Device != mx_dtt2_fil2[i],]
 }
+
+write.csv(x = mx_dtt2_fil_oppo, file = "o1208_mx_dtt2_fil_oppo.csv")
+#which(mx_dtt2$Device == mx_dtt2_fil2[i] | mx_dtt2$Device == mx_dtt2_fil2[2]),]
+##
+mx_dtt2_fil_oppo2 <- mx_dtt2[ -(mx_dtt2_fil_oppo) ]
+write.csv(x = mx_dtt2_fil_oppo2, file = "o1208_mx_dtt2_fil_oppo2.csv")
+
+##
